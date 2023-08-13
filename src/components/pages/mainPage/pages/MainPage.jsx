dc34578facea15a7360baae00f5f682035fe1ee6
Dongjin <ehdwls1638@gmail.com>
2023-08-13 15:46:15 +0900

    feat : delete dummy data

--- src/components/pages/mainPage/pages/MainPage.jsx
+++ src/components/pages/mainPage/pages/MainPage.jsx
@@ -1,46 +1,9 @@
 import StockGrid from "../molecules/StockGrid";
 import { getStocks } from "../../../../services/stocks";
 import { useQuery } from "react-query";
-import {Suspense, useEffect} from "react";
-
-const stocks = [
-  {
-    name: "삼성전자  성전자",
-    stockCode: "005930",
-    price: 80000,
-    change: 1000,
-    recommend: {
-      buy: 45,
-      date: "2021-07-12",
-    },
-  },
-  {
-    name: "삼성전자",
-    stockCode: "005930",
-    price: 80000,
-    change: 1000,
-    recommend: {
-      buy: 80,
-      date: "2021-07-12",
-    },
-  },
-  {
-    name: "삼성전자",
-    stockCode: "005930",
-    price: 80000,
-    change: 1000,
-    recommend: {
-      buy: 73,
-      date: "2021-07-12",
-    },
-  },
-];
-
+import {Suspense} from "react";
 const MainPage = () => {
   const { data, isLoading } = useQuery("stocks", getStocks);
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
   return (
     <div
       className={"main-page flex w-full max-w-[1024px] flex-col items-center"}
