a6ffd9375cdd357ce46ec932dcf09a2378692a95
Dongjin <ehdwls1638@gmail.com>
2023-08-13 18:38:08 +0900

    feat : add dummy datas

---
+++ src/mocks/datas/priceDatas.js
@@ -0,0 +1,147 @@
+export const PRICE_DATAS = [
+  {
+    date: "2023-08-13",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-12",
+    value: 4500,
+    change: -100,
+  },
+  {
+    date: "2023-08-11",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-10",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-09",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-08",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-07",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-06",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-05",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-04",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-03",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-08-02",
+    value: 4500,
+    change: -100,
+  },
+  {
+    date: "2023-08-01",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-31",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-30",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-29",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-28",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-27",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-26",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-25",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-24",
+    value: 4500,
+    change: -100,
+  },
+  {
+    date: "2023-07-23",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-22",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-21",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-20",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-19",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-18",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-17",
+    value: 4500,
+    change: 100,
+  },
+  {
+    date: "2023-07-16",
+    value: 4500,
+    change: 100,
+  },
+];
