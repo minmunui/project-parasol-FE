174cae81d09614f9ded460587f4eb343c6990368
Dongjin <ehdwls1638@gmail.com>
2023-08-22 05:36:20 +0900

    feat : 커서 위치에 따른 툴팁 위치 변경

--- src/hooks/useTooltip.js
+++ src/hooks/useTooltip.js
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMouseLocation from "./useMouseLocation";
+import useWindowWidth from "./useWindowWidth";
 
 const useTooltip = () => {
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
-  const tooltipLocation = useMouseLocation()
+
+  const [tooltipLocation, setTooltipLocation] = useState({ x: 0, y: 0 });
+
+  const mouseLocation = useMouseLocation();
+  const windowWidth = useWindowWidth();
+
+
+
+  useEffect(() => {
+    if (isTooltipVisible) {
+      if (mouseLocation.x > windowWidth / 2) {
+        setTooltipLocation({
+          x: mouseLocation.x - 140,
+          y: mouseLocation.y
+        });
+      } else {
+        setTooltipLocation({
+          x: mouseLocation.x,
+          y: mouseLocation.y
+        });
+      }
+    }
+  }, [mouseLocation, isTooltipVisible, windowWidth]);
+
   const [tooltipContent, setTooltipContent] = useState("");
   const showTooltip = (content) => {
     setIsTooltipVisible(true);
