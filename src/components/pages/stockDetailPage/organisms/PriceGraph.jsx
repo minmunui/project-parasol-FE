95e19263dd96363a2997011e77b22170e6596f0d
Dongjin <ehdwls1638@gmail.com>
2023-08-22 04:27:55 +0900

    feat : y축 추가

--- src/components/pages/stockDetailPage/organisms/PriceGraph.jsx
+++ src/components/pages/stockDetailPage/organisms/PriceGraph.jsx
@@ -1,19 +1,32 @@
 import useFetchPrice from "../../../../hooks/useFetchPrice";
-import GraphBar from "../molecules/GraphBar";
-import { useEffect, useRef } from "react";
+
+import { useEffect, useRef, useState } from "react";
+import GraphBar from "../atoms/GraphBar";
+import { comma } from "../../../../utils/convert";
+import useComponentSize from "../../../../hooks/useComponentSize";
 
 const PriceGraph = ({ stockCode }) => {
   const { prices, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useFetchPrice(stockCode);
 
-  const maxValue = Math.max(...prices.map((price) => price.value));
+  const [maxValue, setMaxValue] = useState(1);
+  const [minValue, setMinValue] = useState(0);
 
   const domObserver = useRef(null);
+  const yAxis = useRef(null);
+
+  const yAxisWidth = useComponentSize(yAxis).width;
+
+  useEffect(() => {
+    if (prices) {
+      setMaxValue(Math.max(...prices.map((price) => price.value)));
+      setMinValue(Math.min(...prices.map((price) => price.value)));
+    }
+  }, [prices]);
 
   useEffect(() => {
     const io = new IntersectionObserver(
       (entries) => {
-        console.log("entries", entries);
         if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
           fetchNextPage();
         }
@@ -32,14 +45,30 @@ const PriceGraph = ({ stockCode }) => {
   return (
     <div
       className={
-        "graph-wrapper flex overflow-hidden w-full max-w-[786px] rounded-md border-2 border-gray-400"
+        "graph-wrapper flex w-full max-w-[786px] overflow-hidden rounded-md border-2 border-gray-400 relative"
       }
     >
       <div
         className={
-          "graph-contents flex h-[400px] w-full min-w-[780px] flex-row-reverse items-end gap-4 overflow-x-scroll whitespace-nowrap p-4"
+          "graph-contents flex h-[400px] w-full flex-row-reverse items-end gap-4 overflow-x-scroll whitespace-nowrap p-4"
         }
       >
+      <div
+          className={
+            "graph-y-axis absolute z-10 flex h-[calc(400px-14px)] translate-y-2 flex-col content-between justify-between"
+          }
+          ref={yAxis}
+      >
+        <div className={"y-axis-item"}>{comma(maxValue)}₩</div>
+        <div className={"y-axis-item"}>
+          {comma((maxValue + minValue) / 2)}₩
+        </div>
+        <div className={"y-axis-item"}>{comma(minValue)}₩</div>
+      </div>
+        <div
+          className={"graph-y-axis-dummy h-full flex-shrink-0 bg-white"}
+          style={{ width: yAxisWidth + 20 +"px" }}
+        ></div>
         {prices.map((value, index) => (
           <GraphBar
             value={value.value}
@@ -56,5 +85,4 @@ const PriceGraph = ({ stockCode }) => {
     </div>
   );
 };
-
 export default PriceGraph;
