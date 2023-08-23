3b3d61b289ee35d7e4e06301f94bb6318990f4be
Dongjin <ehdwls1638@gmail.com>
2023-08-24 00:35:55 +0900

    feat : 엔터 입력을 통한 검색 구현

--- src/components/pages/mainPage/pages/MainPage.jsx
+++ src/components/pages/mainPage/pages/MainPage.jsx
@@ -7,12 +7,21 @@ import StockList from "../organisms/StockList";
 import Toggle from "../../../commons/atoms/Toggle";
 import useInput from "../../../../hooks/useInput";
 import { IoClose } from "react-icons/io5";
-import {filterStocks} from "../../../../utils/calculate";
+import { filterStocks } from "../../../../utils/calculate";
+
 const MainPage = () => {
   const { data, isSuccess } = useQuery("stocks", getStocks);
 
   const [isTable, setIsTable] = useState(true);
   const [filteredStocks, setFilteredStocks] = useState([]);
+  const onSearch = () => {
+    if (value) {
+      const filtered = filterStocks(data, value);
+      setFilteredStocks(filtered);
+    } else {
+      setFilteredStocks(data);
+    }
+  };
 
   useEffect(() => {
     if (isSuccess) {
@@ -31,14 +40,21 @@ const MainPage = () => {
           type={"text"}
           placeholder={"종목명 또는 종목코드를 입력하세요."}
           className={
-            "h-[40px] w-full rounded-l-md border-2 border-gray-300 px-2 focus:border-green-300 focus:outline-none"
+            "h-[40px] w-full rounded-l-md border-2 border-gray-300 px-5 focus:border-green-300 focus:outline-none"
           }
           value={value}
           onChange={onChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              onSearch();
+            }
+          }}
         ></input>
         {value && (
           <button
-            className={"absolute right-28 z-10 h-full text-sm text-gray-500 "}
+            className={
+              "absolute z-10 h-full translate-x-1 text-sm text-gray-500 "
+            }
             onClick={() => {
               setValue("");
               setFilteredStocks(data);
@@ -52,12 +68,7 @@ const MainPage = () => {
             "flex h-[40px] w-[40px] items-center justify-center rounded-r-md border-b-2 border-r-2 border-t-2 border-gray-300 focus:border-green-300 focus:outline-none"
           }
           onClick={() => {
-            if (value) {
-              const filtered = filterStocks(data, value);
-              setFilteredStocks(filtered);
-            } else {
-              setFilteredStocks(data);
-            }
+            onSearch();
           }}
         >
           <FaSearch />
