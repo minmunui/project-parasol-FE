ec9f141e138185d80cf9cae6da42cf9ddf89b311
Dongjin <ehdwls1638@gmail.com>
2023-08-24 01:51:04 +0900

    feat : 로고 클릭 시 새로고침 후 메인 화면으로

--- src/components/layouts/mainLayout/GlobalNavBar.jsx
+++ src/components/layouts/mainLayout/GlobalNavBar.jsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
 import logo from "./images/logo.png";
-
 const GlobalNavBar = () => {
   return (
     <>
@@ -9,13 +7,13 @@ const GlobalNavBar = () => {
           "global-nav-bar sticky flex h-14 items-center justify-between shadow-md"
         }
       >
-        <Link to={"/"} className={"h-full"}>
+        <a href={"/"} className={"h-full"}>
           <img
             src={logo}
             alt={"logo"}
             className={"logo h-full object-contain"}
           />
-        </Link>
+        </a>
         <div className={"gnb-right flex gap-5 px-4"}>
         </div>
       </nav>
