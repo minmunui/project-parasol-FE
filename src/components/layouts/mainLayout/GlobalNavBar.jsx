2cac8b412915f08592f7db98b642e7f3b89bc5db
Dongjin <ehdwls1638@gmail.com>
2023-08-23 21:34:50 +0900

    feat : 불필요한 토글 삭제

--- src/components/layouts/mainLayout/GlobalNavBar.jsx
+++ src/components/layouts/mainLayout/GlobalNavBar.jsx
@@ -1,12 +1,7 @@
 import { Link } from "react-router-dom";
 import logo from "./images/logo.png";
-import Toggle from "../../commons/atoms/Toggle";
-import { useContext } from "react";
-import { FormatContext } from "../../../App";
-import { FaPercent } from "react-icons/fa";
 
 const GlobalNavBar = () => {
-  const { isPercent, setIsPercent } = useContext(FormatContext);
   return (
     <>
       <nav
@@ -22,13 +17,6 @@ const GlobalNavBar = () => {
           />
         </Link>
         <div className={"gnb-right flex gap-5 px-4"}>
-          <Toggle
-            label={<FaPercent />}
-            active={isPercent}
-            onClick={() => {
-              setIsPercent(!isPercent);
-            }}
-          />
         </div>
       </nav>
     </>
