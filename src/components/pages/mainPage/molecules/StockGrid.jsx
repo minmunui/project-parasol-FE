ff0e1aa1398359e09cc82404b63b135080bdee0d
Dongjin <ehdwls1638@gmail.com>
2023-08-23 21:35:45 +0900

    fix : grid 미디어쿼리 사이즈 수정

--- src/components/pages/mainPage/molecules/StockGrid.jsx
+++ src/components/pages/mainPage/molecules/StockGrid.jsx
@@ -2,7 +2,7 @@ import StockCard from "../organisms/StockCard";
 
 const StockGrid = ({ stocks }) => {
   return (
-    <div className="stock-grid grid w-full max-w-[1296px] grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3">
+    <div className="stock-grid grid w-full max-w-[1296px] grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
       {stocks.map((stock, index) => (
         <StockCard stock={stock} key={index} />
       ))}
