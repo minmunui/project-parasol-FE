39fc62f3e16c96403d3b90d6dee362478b5fc3a0
Dongjin <ehdwls1638@gmail.com>
2023-09-02 01:04:02 +0900

    refactor : 불필요한 context 삭제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createContext } from "react";
 import MainPage from "./components/pages/mainPage/pages/MainPage";
 import MainLayout from "./components/layouts/mainLayout/MainLayout";
-import useFormat from "./hooks/useFormat";
 import StockDetailPage from "./components/pages/stockDetailPage/pages/StockDetailPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { SkeletonTheme } from "react-loading-skeleton";
@@ -11,12 +10,10 @@ import Tooltip from "./components/commons/atoms/Tooltip";
 import useTooltip from "./hooks/useTooltip";
 import { HelmetProvider } from "react-helmet-async";
 
-export const FormatContext = createContext(null);
 export const TooltipContext = createContext(null);
 const queryClient = new QueryClient();
 
 function App() {
-  const { isPercent, setIsPercent } = useFormat(true);
   const {
     isTooltipVisible,
     tooltipLocation,
@@ -38,20 +35,18 @@ function App() {
             tooltipContent,
           }}
         >
-          <FormatContext.Provider value={{ isPercent, setIsPercent }}>
-            <SkeletonTheme color={"#202020"} highlightColor={"#aaa"} />
-            <QueryClientProvider client={queryClient}>
-              <BrowserRouter>
-                <Tooltip />
-                <Routes>
-                  <Route element={<MainLayout />}>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/stock/:id" element={<StockDetailPage />} />
-                  </Route>
-                </Routes>
-              </BrowserRouter>
-            </QueryClientProvider>
-          </FormatContext.Provider>
+          <SkeletonTheme color={"#202020"} highlightColor={"#aaa"} />
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <Tooltip />
+              <Routes>
+                <Route element={<MainLayout />}>
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="/stock/:id" element={<StockDetailPage />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </QueryClientProvider>
         </TooltipContext.Provider>
       </HelmetProvider>
     </div>
