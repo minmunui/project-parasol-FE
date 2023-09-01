33099222419b46bdc6ce2b08af34de86f6ec0a67
Dongjin <ehdwls1638@gmail.com>
2023-09-02 01:06:50 +0900

    fix : component size 동적으로 수정

--- src/hooks/useComponentSize.js
+++ src/hooks/useComponentSize.js
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 
 const useComponentSize = (refComponent) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const observer = new ResizeObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      setSize({
+        width: entry.contentRect.width,
+        height: entry.contentRect.height,
+      });
+    });
+  });
+
   const [size, setSize] = useState({ width: 0, height: 0 });
   useEffect(() => {
     if (refComponent.current) {
@@ -8,8 +18,10 @@ const useComponentSize = (refComponent) => {
         width: refComponent.current.offsetWidth,
         height: refComponent.current.offsetHeight,
       });
+      observer.observe(refComponent.current);
     }
-  }, [refComponent]);
+  }, [observer, refComponent]);
+
   return size;
 };
 
